10c8454cab582fc34dea56c81e39bfec17b8d0a1
Bandi86 <susutechno@gmail.com>
2023-09-14 14:23:32 +0200

    config fix

--- _learning/freecodecamp/nextjs-appwrite-fullstack-course/next.config.js
+++ _learning/freecodecamp/nextjs-appwrite-fullstack-course/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverRuntimeConfig: {
-    reactStrictMode: true,
-    env: {
-      MONGO_URI: 'mongodb+srv://susutechno:sARJxldRlX1aQvfv@cluster0.ivay4xh.mongodb.net/app?retryWrites=true&w=majority'
-    },
-  },
-  publicRuntimeConfig: {
-    staticFolder: '/static',
-  },
+
 };
 
 module.exports = nextConfig;

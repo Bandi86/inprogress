6c8bdea2b5127af3934f6f7d864953d527a7c91d
Bandi86 <susutechno@gmail.com>
2024-01-24 18:58:58 +0100

    Remove unused code and fix addToCart function

--- bookshop/frontend/utils/addToCart.ts
+++ bookshop/frontend/utils/addToCart.ts
@@ -1,19 +1,14 @@
 import { cartApi } from '@/constants/api'
 import { Book } from '@/types/book'
 import axios from 'axios'
-import { cookies } from 'next/headers'
+//import { cookies } from 'next/headers'
 
 const addToCart = async (book: Book) => {
-  const cookieStore = cookies()
-  const token = cookieStore.get('jwt')
+ /*  const cookieStore = cookies()
+  const token = cookieStore.get('jwt') */
 
   // get the full book from props what added the user to cart
-  const res = await axios.post(cartApi, book, {
-    // Add a comma here
-    headers: {
-      Authorization: `Bearer ${token}`, // Declare the 'token' variable or replace it with the actual token value
-    }, // Add a comma here
-  })
+  const res = await axios.post(cartApi, book)
   if (res.status === 200) {
     console.log('book added to cart')
   } else {
